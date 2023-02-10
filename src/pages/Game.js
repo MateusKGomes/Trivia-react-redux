2f22f87166212cb37be918526071a7269bc15a3c
Mateus Kulik <mateuskulik30@gmail.com>
2023-02-10 17:00:34 -0300

    requisito 6 concluído

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MultipleQuestion from '../components/MultipleQuestion';
 import Header from './Header';
 
 class Game extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <div>
           <Header />
-          <MultipleQuestion />
+          <MultipleQuestion history={ history } />
         </div>
 
       </div>
     );
   }
 }
-
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 export default Game;
