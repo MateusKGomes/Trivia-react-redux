8d68ec298fed7cbedbc9c6dca657dd882763050c
Mateus Kulik <mateuskulik30@gmail.com>
2023-02-13 17:35:38 -0300

    Requisito 17 concluído

---
+++ src/tests/Feedback.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { screen, act } from "@testing-library/react";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux"
+import Feedback from "../pages/Feedback";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+import user from "../redux/reducers/user";
+
+
+describe ("Testa a tela de Feedback", () => {
+    it ('Verifica se os itens do Feedback estão na tela ', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        act(() => {
+            history.push('/feedback');
+          });
+        const { location: { pathname }} = history;
+        expect(pathname).toBe('/feedback');
+       const playAgain = screen.getByRole('button', {  name: /play again/i})
+       expect(playAgain).toBeInTheDocument();
+       const ranking = screen.getByRole('button', {  name: /ranking/i});
+       expect(ranking).toBeInTheDocument();
+       const score = screen.getByTestId('feedback-total-score')
+       expect(score).toBeInTheDocument();
+    });
+    it('testa o click de play again', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        act(() => {
+            history.push('/feedback');
+          });
+          const playAgain = screen.getByRole('button', {  name: /play again/i})
+          userEvent.click(playAgain);
+          const { location: { pathname }} = history;
+          expect(pathname).toBe('/');
+    })
+    it('testa o evento do ranking ', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        act(() => {
+            history.push('/feedback');
+          });
+          const ranking = screen.getByRole('button', {  name: /ranking/i});
+          userEvent.click(ranking);
+          const { location: { pathname }} = history;
+          expect(pathname).toBe('/ranking');    
+        })
+});
--- src/tests/Game.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-
-it('renders without crashing', () => {
-    const history = { push: () => {} };
-    const { getByText } = render(<Game history={history} />);
-    expect(getByText('Header')).toBeInTheDocument();
-    expect(getByText('MultipleQuestion')).toBeInTheDocument();
-  });
