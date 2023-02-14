3d0b0376be9c0e8df205256ebbae2a0d2bb3a510
Andreyrodrigues <andrey.rspa@gmail.com>
2023-02-13 22:34:04 -0300

    Requisito 21

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,8 +1,19 @@
 // import { render } from '@testing-library/react';
 
+import { screen } from "@testing-library/react";
+import Game from "../pages/Game"
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux"
+
 // it('renders without crashing', () => {
 //     const history = { push: () => {} };
 //     const { getByText } = render(<Game history={history} />);
 //     expect(getByText('Header')).toBeInTheDocument();
 //     expect(getByText('MultipleQuestion')).toBeInTheDocument();
 //   });
+
+describe('Teste da página de Game', () => {
+    it('Verifica se existe heading level 1', () => {
+        renderWithRouterAndRedux(<Game />);
+        const testImage = screen.getByRole('heading', { level: 1 })
+    })
+})
